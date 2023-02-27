fd0034bb289f6a7f85a9153e7edf5efb93f0be93
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-02-27 11:40:06 +0100

    definite le principali funzioni da usare in Backtracking.py

--- src/Backtracking.py
+++ src/Backtracking.py
@@ -1,2 +1,17 @@
 def backtrack(graph, assignment):
     return
+
+def check_assignment_complete(assignment):
+    return
+
+def select_unassigned_variable(graph, assignment):
+    return
+
+def order_domain_values(graph, var, assignment):
+    return
+
+def forward_checking(graph, var, assignment):
+    return
+
+def mac(graph, var, assignment):
+    return
