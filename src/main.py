9a7e64bbd6addb2f25919644e95ba09a40a5e820
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-03-07 11:26:03 +0100

    testing MAC e FC

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from Graph import Graph as Graph
 from Graph import Node as Node
 
-
 # CONST VALUES #
 N_NODES = 5
 SAVE_GRAPH = False
+
 
 def main():
     graph = Graph(N_NODES, SAVE_GRAPH)
@@ -14,8 +14,9 @@ def main():
     print("RANDOM: ", random_node1.get_label())
 
     graph.generate_edges(random_node1)
-    print("FC: ", graph.backtracking("ForwardChecking"))
 
+    print("FC: ", graph.backtracking("ForwardChecking"))
+    # print("Mac: ", graph.backtracking("Mac"))
 
 
 if __name__ == "__main__":
