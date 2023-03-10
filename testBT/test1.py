4e91f31b06cc4fa3b03e62dda7aafffe3f4b84c8
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2023-03-10 11:56:50 +0100

    funziona il test sul tempo ma i risultati sono tutti sballati

--- testBT/test1.py
+++ testBT/test1.py
@@ -1,30 +1,47 @@
+import time
+
 import src.Graph as Graph
 import src.Backtracking as BT
 import matplotlib.pyplot as plt
 
 # CONST VALUES #
-N_NODES = 4
+N_NODES = 10
 ANIMATION = False
 
 
-def generate_graph(ax1_title, ax2_title, ax1_data, ax2_data):
-    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-    ax1.set_title(ax1_title)
-    ax2.set_title(ax2_title)
+# TODO: FAI CON 4 COLORI!!!!
 
-    for x_1, y_1 in ax1_data.items():
-        ax1.plot(x_1, y_1)
+def generate_graph(ax1_title, ax2_title, ax3_title, ax4_title, ax1_data, ax2_data, ax3_data, ax4_data):
+    # Creiamo una figura con quattro assi verticali
+    fig, axs = plt.subplots(4, sharex=True)
 
-    for x_2, y_2 in ax2_data.items():
-        ax2.plot(x_2, y_2)
+    # Impostiamo i titoli per gli assi
+    axs[0].set_title(ax1_title)
+    axs[1].set_title(ax2_title)
+    axs[2].set_title(ax3_title)
+    axs[3].set_title(ax4_title)
 
-    ax1.plot(2, 4)
-    ax2.plot(1, 5)
+    # Estrapoliamo i dati dagli argomenti e li inseriamo in liste separate
+    data1_x, data1_y = list(ax1_data.keys()), list(ax1_data.values())
+    data2_x, data2_y = list(ax2_data.keys()), list(ax2_data.values())
+    data3_x, data3_y = list(ax3_data.keys()), list(ax3_data.values())
+    data4_x, data4_y = list(ax4_data.keys()), list(ax4_data.values())
+
+    # Tracciamo i dati sui subplot
+    axs[0].plot(data1_x, data1_y)
+    axs[1].plot(data2_x, data2_y)
+    axs[2].plot(data3_x, data3_y)
+    axs[3].plot(data4_x, data4_y)
+
+    # Mostriamo il grafico
     plt.show()
 
 
 # Confronta tempi per risoluzione con mappa random e mappa simmetrica
 def time_performance_test():
+    ran_FC_data, sym_FC_data = dict(), dict()
+    ran_MAC_data, sym_MAC_data = dict(), dict()
+
     for i in range(2, N_NODES):
         random_graph = Graph.Graph(i, ANIMATION, False)
         sym_graph = Graph.Graph(i, ANIMATION, True)
@@ -39,9 +56,31 @@ def time_performance_test():
         random_graph.generate_edges(starting_node_random)
         sym_graph.generate_edges(starting_node_sym)
 
+        # -------- FC --------#
+        start_time_ran_FC = time.time()
+        print("Random graph solution FC: ", random_graph.backtracking("ForwardChecking"))
+        end_time_ran_FC = time.time()
+
+        start_time_sym_FC = time.time()
+        print("Symmetric graph solution FC: ", sym_graph.backtracking("ForwardChecking"))
+        end_time_sym_FC = time.time()
+
+        # ------- MAC --------#
+        start_time_ran_MAC = time.time()
+        print("Random graph solution MAC: ", random_graph.backtracking("Mac"))
+        end_time_ran_MAC = time.time()
+
+        start_time_sym_MAC = time.time()
+        print("Symmetric graph solution MAC: ", sym_graph.backtracking("Mac"))
+        end_time_sym_MAC = time.time()
+
+        ran_FC_data[i] = (end_time_ran_FC - start_time_ran_FC)
+        ran_MAC_data[i] = (end_time_ran_MAC - start_time_ran_MAC)
+
+        sym_FC_data[i] = (end_time_sym_FC - start_time_sym_FC)
+        sym_MAC_data[i] = (end_time_sym_MAC - start_time_sym_MAC)
+
         del random_graph, sym_graph
 
-    data1 = {3: 4}
-    data2 = {6: 2}
-
-    generate_graph("Random Graph", "Symmetric Graph", data1, data2)
+    generate_graph("Random Graph FC", "Random Graph MAC", "Symmetric Graph FC", "Symmetric Graph MAC",
+                   ran_FC_data, ran_MAC_data, sym_FC_data, sym_MAC_data)
